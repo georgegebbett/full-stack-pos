b1c231dcd42dd969525b0fa15c4ef6a377b5f000
George Gebbett <george.gebbett1@gmail.com>
2021-10-18 10:03:02 +0100

    eslint issues in closedTables.jsx resolved

--- src/client/views/closedTables/closedTables.jsx
+++ src/client/views/closedTables/closedTables.jsx
@@ -9,28 +9,14 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useAuth } from '../../controllers/use-auth';
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab';
 import { Link as RouterLink } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
-const LinkBehavior = React.forwardRef((props, ref) => (
-  <RouterLink ref={ref} to="/" {...props} role={undefined} />
-));
-
-function toFixed(num, fixed) {
-  const re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
-  return num.toString().match(re)[0];
-}
-
 
 export default function ClosedTables() {
-  const auth = useAuth();
   const [tables, setTables] = useState([]);
 
   useEffect(() => {
-
     const getTableData = async () => {
       const { data } = await axios.get('/api/tables?open=false');
       setTables(data);
