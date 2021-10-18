edd0af1ccbf1ec98a10c77364303fc2dc21e0f37
George Gebbett <george.gebbett1@gmail.com>
2021-10-18 10:04:33 +0100

    about.jsx improved

--- src/client/views/about/about.jsx
+++ src/client/views/about/about.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { TextField, Card, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useAuth } from '../../controllers/use-auth';
 
 function About() {
   const auth = useAuth();
   return (
     <div className="homeDiv">
+      <Typography variant="h2">
+        FullStackPos - (c) 2021 George Gebbett
+      </Typography>
       <p>
         Authenticated user:
         {auth.user}
